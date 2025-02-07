d2193cf676c7df6321631ad330377182d509646a
Hasun Nilupul <hasunnilupul16@gmail.com>
2025-02-08 00:37:47 +0530

    uploadthing added

--- src/app/_components/TopNav.tsx
+++ src/app/_components/TopNav.tsx
@@ -1,17 +1,34 @@
+"use client";
+
 import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
-import { User } from "@clerk/nextjs/server";
+import { useRouter } from "next/navigation";
+import { UploadButton } from "~/utils/uploadthing";
 
 export default function TopNav() {
+  const router = useRouter();
+
   return (
     <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold">
       <div>Gallery</div>
 
-      <div>
+      <div className="flex flex-row">
         <SignedOut>
           <SignInButton />
         </SignedOut>
 
         <SignedIn>
+          <UploadButton
+            endpoint="imageUploader"
+            onClientUploadComplete={(res) => {
+              // Do something with the response
+              console.log("Files: ", res);
+              router.refresh();
+            }}
+            onUploadError={(error: Error) => {
+              // Do something with the error.
+              alert(`ERROR! ${error.message}`);
+            }}
+          />
           <UserButton />
         </SignedIn>
       </div>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,13 @@
-import "~/styles/globals.css";
-
 import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
-import {
-  ClerkProvider,
-} from "@clerk/nextjs";
+import { ClerkProvider } from "@clerk/nextjs";
+import { NextSSRPlugin } from "@uploadthing/react/next-ssr-plugin";
+import { extractRouterConfig } from "uploadthing/server";
 import TopNav from "~/app/_components/TopNav";
+
+import "~/styles/globals.css";
+import "@uploadthing/react/styles.css";
+import { uploadThingFileRouter } from "./api/uploadthing/core";
 
 export const metadata: Metadata = {
   title: "T3 Gallery",
@@ -18,6 +20,15 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <ClerkProvider>
+      <NextSSRPlugin
+        /**
+         * The `extractRouterConfig` will extract **only** the route configs
+         * from the router to prevent additional information from being
+         * leaked to the client. The data passed to the client is the same
+         * as if you were to fetch `/api/uploadthing` directly.
+         */
+        routerConfig={extractRouterConfig(uploadThingFileRouter)}
+      />
       <html lang="en" className={`${GeistSans.variable}`}>
         <body className="flex flex-col gap-4">
           <TopNav />
